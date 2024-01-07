513ec8f9b71d61c0e160f08aa89c46d747c1899a
112frosta <112frosta@proton.me>
2024-01-07 14:24:10 +0100

    Add index.ts file with Discord.js client setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Client } from "discord.js";
+
+import initializeCommands from "./handlers/commandHandler";
+
+require("dotenv").config();
+
+const client = new Client({ intents: ["MessageContent"] });
+
+client.once("ready", async () => {
+  await initializeCommands();
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.login(process.env.BOT_TOKEN);
+
+export { client };
