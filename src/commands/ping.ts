5acbf2278d40b2279ca114e42ccb0c6affa9ed5d
112frosta <112frosta@proton.me>
2024-01-07 14:24:19 +0100

    Add ping command implementation

---
+++ src/commands/ping.ts
@@ -0,0 +1,12 @@
+import type { CommandInteraction } from "discord.js";
+import { SlashCommandBuilder } from "discord.js";
+
+const config = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("Replies with ping!");
+
+const run = async (interaction: CommandInteraction) => {
+  await interaction.reply("Pong!");
+};
+
+export { config, run };
